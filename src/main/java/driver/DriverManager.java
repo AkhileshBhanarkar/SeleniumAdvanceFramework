a89ee4c6beb233c96d89bed26789662fa94c7611
akhileshbhanarkar <akhileshbhanarkar45@gmail.com>
2025-11-16 19:39:07 +0530

    minor changes

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -1,5 +1,6 @@
 package driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -25,16 +26,19 @@ public class DriverManager {
 
         switch (browser.toLowerCase()){
             case "chrome":
+                WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
                 if(headless) chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
                 driver.set(new ChromeDriver(chromeOptions));
                 break;
             case "firefox":
+                WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
                 if(headless) firefoxOptions.addArguments("-headless");
                 driver.set(new FirefoxDriver(firefoxOptions));
                 break;
-             case "edge":
+            case "edge":
+                WebDriverManager.edgedriver().setup();
                  EdgeOptions edgeOptions = new EdgeOptions();
                  if (headless) edgeOptions.addArguments("--headless");
                  driver.set(new EdgeDriver(edgeOptions));
